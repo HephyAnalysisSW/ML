186e28bb1bf36259592ea4a3cee13b893ffa2181
rdgithubname <43813119+rdgithubname@users.noreply.github.com>
2020-10-21 15:45:22 +0200

    Add files via upload

--- students/Max/Multiclassification.py
+++ students/Max/Multiclassification.py
@@ -86,15 +86,114 @@ for i, j in enumerate(VARS):
       density=True, histtype=u'step', linewidth=2, alpha=0.8)
     a, b, c = plt.hist(df['WZ'][j], bins=30, range=(minimum, maximum), label='WZ',
       density=True, histtype=u'step', linewidth=2, alpha=0.5)
-    plt.title(j, fontsize=20)
+    xlab = j
+    xlab = xlab.replace('mva_', '')
+    xlab = xlab.replace('_', ' ')
+    plt.title(xlab, fontsize=20)
     plt.ylabel('N', fontsize=20)
-    plt.xlabel('Energy [GeV]', fontsize=20)
+    plt.xlabel(xlab, fontsize=20)
     plt.legend(fontsize=15)
     plt.savefig('plots/' + j + '.png')
     print(i+1, ' of ', len(VARS), ': ', 'plots/' + j)
+    plt.close()
 
 
 # bis hier 16.10.
+
+
+NDIM = len(VARS)
+
+df['TWZ']['isSignal'] = np.ones(len(df['TWZ'])) * 0
+df['TTZ']['isSignal'] = np.ones(len(df['TTZ'])) * 1
+df['WZ']['isSignal'] = np.ones(len(df['WZ'])) * 2
+df_all = pd.concat([df['TWZ'], df['TTZ'], df['WZ']])
+dataset = df_all.values
+X = dataset[:,0:NDIM]
+Y = dataset[:,NDIM]
+
+
+
+# splitting the 3 Data sets into Train and Test Parts
+from sklearn.model_selection import train_test_split
+
+X_train_val, X_test, Y_train_val, Y_test = train_test_split(X, Y, test_size=0.2, random_state=7)
+
+# preprocessing: standard scalar
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler().fit(X_train_val)
+X_train_val = scaler.transform(X_train_val)
+X_test = scaler.transform(X_test)
+
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.neural_network import MLPClassifier
+
+clf = OneVsOneClassifier(MLPClassifier(solver='adam', alpha=1e-5,
+    hidden_layer_sizes=(10,10), random_state=7, early_stopping=True,
+    max_iter=100000, activation='logistic'))
+clf.fit(X_train_val, Y_train_val)
+
+print(clf.predict(X_test[:20]))
+print(Y_test[:20])
+testlen = len(Y_test)
+falsch = 0
+richtig = 0
+predicted = clf.predict(X_test)
+for i in range(0, testlen):
+    if Y_test[i] == predicted[i]:
+        richtig += 1
+    else:
+        falsch += 1
+
+
+skit_accuracy = richtig/testlen
+print('Richtig Klassifiziert:', skit_accuracy)
+print(richtig, falsch, testlen)
+print(clf.predict(X_test))
+
+
+#######################################################################################
+# Compare with Keras One versus All:
+#######################################################################################
+
+# TTZ + TWZ:
+'''
+arr1 = np.zeros((len(df['TTZ']), 3))
+arr1[:,0] = np.ones((len(df['TTZ'])))
+df['TTZ']['isSignal'] = arr1
+
+arr2 = np.zeros((len(df['TTZ']), 3))
+arr2[:,1] = np.ones((len(df['TTZ'])))
+df['TWZ']['isSignal'] = arr2
+
+arr3 = np.zeros((len(df['TTZ']), 3))
+arr3[:,0] = np.ones((len(df['TTZ'])))
+df['WZ']['isSignal'] = arr3'''
+
+df['TWZ']['isSignal'] = np.ones(len(df['TWZ'])) * 0
+df['TTZ']['isSignal'] = np.ones(len(df['TTZ'])) * 1
+df['WZ']['isSignal'] = np.ones(len(df['WZ'])) * 2
+
+
+
+df_all = pd.concat([df['TTZ'], df['TWZ'], df['WZ']])
+dataset = df_all.values
+X = dataset[:,0:NDIM]
+Y = dataset[:,NDIM]
+Ymat = np.zeros((len(Y),3))
+for i, y in enumerate(Y):
+    if y == 0:
+        Ymat[i] = np.array([1,0,0])
+    elif y == 1:
+        Ymat[i] = np.array([0,1,0])
+    elif y == 2:
+        Ymat[i] = np.array([0,0,1])
+    else:
+        print('ERROR')
+
+Y = Ymat
+
+X_train_val, X_test, Y_train_val, Y_test = train_test_split(X, Y, test_size=0.2, random_state=7)
+
 
 
 # baseline keras model
@@ -104,70 +203,37 @@ from keras.layers import Input, Activation, Dense, Convolution2D, MaxPooling2D, 
 from keras.layers import BatchNormalization
 from keras.utils import np_utils
 
-NDIM = len(VARS)
-inputs = Input(shape=(NDIM,), name = 'input')  
-outputs = Dense(1, name = 'output', kernel_initializer='normal', activation='sigmoid')(inputs)
-
-# creae the model
-# choose the default model or the one with 1 hidden layer
-model = Model(inputs=inputs, outputs=outputs)
-# or:
-model = Sequential([Flatten(input_shape=(NDIM, 1)),
-                    Dense(NDIM, activation='sigmoid'),
-                    Dense(1,kernel_initializer='normal', activation='sigmoid')])
-
-# or: 
-#with Batch Normalisation
 
 model = Sequential([Flatten(input_shape=(NDIM, 1)),
                     BatchNormalization(),
-                    Dense(NDIM, activation='sigmoid'),
-                    Dense(NDIM, activation='sigmoid'),
-                    Dense(NDIM, activation='sigmoid'),
-                    Dense(1,kernel_initializer='normal', activation='sigmoid')])
-
-
-# compile the model
+                    Dense(NDIM*5, activation='sigmoid'),
+                    Dense(NDIM*5, activation='sigmoid'),
+                    Dense(NDIM*5, activation='sigmoid'),
+                    Dense(3,kernel_initializer='normal', activation='sigmoid')])
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-# print the model summary
 model.summary()
-
-
-df_all = df['data']
-#df_all = pd.concat([df['VV'],df['bkg']])
-dataset = df_all.values
-X = dataset[:,0:NDIM]
-Y = dataset[:,NDIM]
-
-from sklearn.model_selection import train_test_split
-X_train_val, X_test, Y_train_val, Y_test = train_test_split(X, Y, test_size=0.2, random_state=7)
-
-# preprocessing: standard scalar
-from sklearn.preprocessing import StandardScaler
-#scaler = StandardScaler().fit(X_train_val)
-#X_train_val = scaler.transform(X_train_val)
-#X_test = scaler.transform(X_test)
-
 
 
 import tensorflow as tf
 
-callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=2)
 
 history = model.fit(X_train_val, 
                     Y_train_val, 
                     epochs=1000, 
-                    batch_size=1024*4,
+                    batch_size=1024,
                     #verbose=0, # switch to 1 for more verbosity, 'silences' the output
                     callbacks=[callback],
                     #validation_split=0.25
                     validation_data=(X_test,Y_test)
                    )
 
+test_loss, test_acc = model.evaluate(X_test,  Y_test, verbose=2)
 
+print('\nTest accuracy:', test_acc)
+print('Skit accuracy:', skit_accuracy)
 
-
-#%matplotlib inline
+'''
 # plot loss vs epoch
 plt.figure(figsize=(15,10))
 ax = plt.subplot(2, 2, 1)
@@ -204,3 +270,5 @@ plt.show()
 
 df_all['dense'] = model.predict(X) # add prediction to array
 print(df_all.iloc[:5])
+
+'''
